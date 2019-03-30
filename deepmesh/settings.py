bff240decfd07bd5bd28090e2e0f64aef0c26271
Colton-Bishop <cmbishop@princeton.edu>
2019-03-29 23:17:32 -0400

    Design and Layout

--- deepmesh/settings.py
+++ deepmesh/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
-    '/model/static/',
+    'model/static/',
 ]
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'staticfiles/'
