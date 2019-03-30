aa7bbbf0c51fb7e52f6034d3739657d0eda952b5
Colton-Bishop <cmbishop@princeton.edu>
2019-03-29 23:24:59 -0400

    Design and Layout

--- deepmesh/settings.py
+++ deepmesh/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATICFILES_DIRS = [
+    '/static/',
     os.path.join(BASE_DIR, "static"),
     'model/static/',
 ]
