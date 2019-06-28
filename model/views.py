3351a75d653c77434f7fa1e9e897af981398af0f
Colton-Bishop <cmbishop@princeton.edu>
2019-06-28 11:28:18 -0400

    Authentication Bug

--- model/views.py
+++ model/views.py
@@ -23,9 +23,9 @@ password='********'
 
 def upload_pic(request):
 
-	ssh=paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(hostname,port,username,password)
+	# ssh=paramiko.SSHClient()
+	# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	# ssh.connect(hostname,port,username,password)
 
 	template = loader.get_template('model/model.html')
 	image = forms.ImageField()
